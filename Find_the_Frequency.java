57639d044ac49f85c54078d64e0fad3d5b7b55d6
RAJU KUMAR <97028776+raju-kumar21@users.noreply.github.com>
2022-02-08 22:18:43 +0530

    Create Find_the_Frequency.java

---
+++ Find_the_Frequency.java
@@ -0,0 +1,62 @@
+/*Given a vector of N positive integers and an integer X. The task is to find the frequency of X in vector.*/
+
+import java.io.*;
+import java.util.*;
+
+class FastReader{ 
+    BufferedReader br; 
+    StringTokenizer st; 
+
+    public FastReader(){ 
+        br = new BufferedReader(new InputStreamReader(System.in)); 
+    } 
+
+    String next(){ 
+        while (st == null || !st.hasMoreElements()){ 
+            try{ st = new StringTokenizer(br.readLine()); } catch (IOException  e){ e.printStackTrace(); } 
+        } 
+        return st.nextToken(); 
+    } 
+
+    String nextLine(){ 
+        String str = ""; 
+        try{ str = br.readLine(); } catch (IOException e) { e.printStackTrace(); } 
+        return str; 
+    } 
+    
+    Integer nextInt(){
+        return Integer.parseInt(next());
+    }
+} 
+    
+class GFG {
+    public static void main(String args[]) throws IOException {
+        FastReader sc = new FastReader();
+        PrintWriter out = new PrintWriter(System.out);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            Solution ob = new Solution();
+            int N = sc.nextInt(), A[] = new int[N];
+    	    for(int i = 0;i<N;i++){
+    	        A[i] = sc.nextInt();
+    	    }
+    	    int x = sc.nextInt();
+    	    out.println(ob.findFrequency(A, x));
+        }
+        out.flush();
+    }
+}
+
+class Solution{
+    int findFrequency(int A[], int x){
+
+   int count = 0;
+   for(int i=0;i<A.length; i++)
+   {
+       if(A[i]==x)
+       count++;
+       
+   }
+   return count;   
+    }
+}
