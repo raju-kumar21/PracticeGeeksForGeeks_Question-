1bf5dfd7dc40e7365ebd653fc800076f151c4c0b
RAJU KUMAR <97028776+raju-kumar21@users.noreply.github.com>
2022-02-08 22:02:37 +0530

    Create Peak_element.java

---
+++ Peak_element.java
@@ -0,0 +1,64 @@
+/*An element is called a peak element if its value is not smaller than the value of its adjacent elements(if they exists).
+Given an array arr[] of size N, find the index of any one of its peak elements.
+Note: The generated output will always be 1 if the index that you return is correct. Otherwise output will be 0. */
+
+
+
+
+
+
+import java.util.*;
+class PeakElement{
+	public static void main(String[] args){
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		while(t-->0){
+			int n=sc.nextInt();
+			int[] a=new int[n];
+		    int[] tmp=new int[n];
+			for(int i=0;i<n;i++){
+				a[i]=sc.nextInt();
+				tmp[i] = a[i];
+			}
+		    
+		    int f=0;
+			int A=(new Solution()).peakElement(tmp,n);
+			
+			if(A<0 && A>=n)
+			    System.out.println(0);
+			else
+			{
+    			if(n==1 && A==0)
+    			    f=1;
+    			else if(A==0 && a[0]>=a[1])
+    			    f=1;
+    			else if(A==n-1 && a[n-1]>=a[n-2])
+    			    f=1;
+    			else if(A >= 0 && A < n && a[A] >=a[A+1] && a[A]>= a[A-1])
+    			    f=1;
+    			else
+    			    f=0;
+			    System.out.println(f);
+			}
+		}
+	}
+}
+
+
+class Solution
+{
+	public int peakElement(int[] arr,int n)
+    {
+       //add code here.
+      int ind = -1;
+        int l = -1;
+        
+        for(int i=0; i<n; i++){
+            if(arr[i]>l){
+                ind = i;
+                l = arr[i];
+            }
+        }
+        return ind;
+    }
+}
